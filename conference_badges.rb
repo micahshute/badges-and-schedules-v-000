e95f5b506673d34c5107a8f4cb452d6897be6a3e
Micah Shute <micah.shute@gmail.com>
2019-04-23 21:58:55 -0500

    finished study group

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,2 +1,21 @@
 # Write your code here.
+ def badge_maker (name)
+  return "Hello, my name is #{name}."
+ end
 
+
+ def batch_badge_creator(attendees)
+  attendees.map {|name| badge_maker(name)}
+ end
+
+ def assign_rooms(attendees)
+  attendees.map.with_index do |name, index|
+    "Hello, #{name}! You'll be assigned to room #{index + 1}!"
+  end
+ end
+
+ def printer(attendees)
+    batch_badge_creator(attendees).each {|name| puts name}
+    assign_rooms(attendees).each {|name| puts name}
+ end
+  
